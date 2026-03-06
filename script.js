4b9f1f85afecd360eafd48bd0b0e424b3d6bc95c
SHShadhin <shshadhin0@gmail.com>
2026-03-06 22:56:14 +0600

    navbar added

---
+++ script.js
@@ -0,0 +1,17 @@
+document.getElementById('sign-btn').addEventListener('click', function () {
+  const textInput = document.getElementById('text-input');
+  const userInput = textInput.value;
+
+  const passInput = document.getElementById('pass-input');
+  const password = passInput.value;
+
+  // console.log(userInput,password)
+  if (userInput == 'admin' && password == 'admin123') {
+    alert('login success')
+    window.location.assign("index2.html");
+
+  } else {
+    alert('login failed')
+    return;
+  }
+})
