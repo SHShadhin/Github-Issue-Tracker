5f36112ff0b1e05a2380d38d82dacd0ae4999934
SHShadhin <shshadhin0@gmail.com>
2026-03-07 17:24:22 +0600

    show modal added

--- script2.js
+++ script2.js
@@ -4,13 +4,13 @@ btns.forEach(button => {
   // console.log(button)
   button.addEventListener('click', function () {
     btns.forEach(b => {
-      b.classList.remove('btn-primary')
-      b.classList.add('btn-outline')
-    })
+      b.classList.remove('btn-primary');
+      b.classList.add('btn-outline');
+    });
     button.classList.add('btn-primary');
     button.classList.remove('btn-outline');
-  })
-})
+  });
+});
 
 // load all issues
 const loadAllIssues = () => {
@@ -22,19 +22,17 @@ const loadAllIssues = () => {
     });
 };
 
-
 // display all issues in main page
 
 const issueCount = document.getElementById('isssue-count'); // issue count ke id diye dhore ana hoise
 
-const displayAllIssues = (allIssues) => {
+const displayAllIssues = allIssues => {
   // console.log(allIssues)
   const allIssueContainer = document.getElementById('all-issue-container');
   allIssueContainer.innerHTML = '';
   issueCount.textContent = allIssues.length; // issues count er man update kora hoise
 
   allIssues.forEach(issues => {
-
     // change border color of issues
     if (issues.status.toLowerCase() === 'open') {
       borderColor = 'border-t-6 border-green-500 rounded-lg';
@@ -46,7 +44,7 @@ const displayAllIssues = (allIssues) => {
     const div = document.createElement('div');
     div.classList.add('card', 'w-96', 'h-full', 'bg-base-100', 'shadow-sm');
     div.innerHTML = `
-        <div class="card-body ${borderColor}">
+        <div class="card-body ${borderColor}" onclick="openModal(${issues.id})">
           <div class="flex justify-between items-center ">
             <div>
               <img src="assets/Open-Status.png" alt="">
@@ -80,13 +78,11 @@ const displayAllIssues = (allIssues) => {
 
 loadAllIssues();
 
-
 // issues filtering
 // all button
 const allbtn = document.getElementById('all-btn');
 allbtn.addEventListener('click', () => {
   displayAllIssues(allIssues);
-  
 });
 // Open button
 const openBtn = document.getElementById('open-btn');
@@ -105,3 +101,52 @@ closeBtn.addEventListener('click', () => {
   );
   displayAllIssues(closedIssues);
 });
+
+// "status": "success",
+// "message": "Issue fetched successfully",
+// "data": {
+// "id": 33,
+// "title": "Add bulk operations support",
+// "description": "Allow users to perform bulk actions like delete, update status on multiple items at once.",
+// "status": "open",
+// "labels": [
+// "enhancement"
+// ],
+// "priority": "low",
+// "author": "bulk_barry",
+// "assignee": "",
+// "createdAt": "2024-02-02T10:00:00Z",
+// "updatedAt": "2024-02-02T10:00:00Z"
+// }
+// for modal details
+const issueModal = document.getElementById('issue_modal');
+const modalTitle = document.getElementById('modalTitle');
+const modalStatus = document.getElementById('modalStatus');
+const assignModal = document.getElementById('assignModal');
+const authorModal = document.getElementById('authorModal');
+const modalDesc = document.getElementById('modalDesc');
+const bugModal = document.getElementById('bugModal');
+const bugModal2 = document.getElementById('bugModal2');
+const modalPriority = document.getElementById('modalPriority');
+
+async function openModal(issueId) {
+  // console.log(issueId);// check issue id pacci ki na
+  const res = await fetch(
+    `https://phi-lab-server.vercel.app/api/v1/lab/issue/${issueId}`,
+  );
+  const data = await res.json();
+  const issueDetails = data.data;
+  // console.log(issueDetails) //check all data paisi ki na
+
+  // set modal inner html;
+  modalTitle.innerText = issueDetails.title;
+  modalStatus.innerText = issueDetails.status;
+  assignModal.innerText = issueDetails.assignee;
+  authorModal.innerText = issueDetails.author;
+  modalDesc.innerText = issueDetails.description;
+  modalPriority.innerText = issueDetails.priority;
+  bugModal.innerText = issueDetails.labels[0];
+  bugModal2.innerText = issueDetails.labels[1];
+
+   issueModal.showModal(); // modal show koraisi
+}
