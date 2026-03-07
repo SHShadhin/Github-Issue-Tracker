322a33c6e030971a63b5e61a4be1020a131b734a
SHShadhin <shshadhin0@gmail.com>
2026-03-07 14:26:04 +0600

    issues filtering done

--- script2.js
+++ script2.js
@@ -12,23 +12,20 @@ btns.forEach(button => {
   })
 })
 
-// issues filtering 
-const openBtn = document.getElementById('open-btn');
-const closeBtn = document.getElementById('close-btn');
-
+// load all issues
 const loadAllIssues = () => {
   fetch('https://phi-lab-server.vercel.app/api/v1/lab/issues')
     .then(res => res.json())
-    .then(data => displayAllIssues(data.data));
+    .then(data => {
+      allIssues = data.data;
+      displayAllIssues(allIssues); // initial load এ সব দেখাবে
+    });
 };
-
-const displayAllIssues = allIssues => {
+// display all issues in main page
+const displayAllIssues = (allIssues) => {
   // console.log(allIssues)
   const allIssueContainer = document.getElementById('all-issue-container');
-
-  //   "title": "Fix navigation menu on mobile devices",
-  // "description": "The navigation menu doesn't collapse properly on mobile devices. Need to fix the responsive behavior.",
-  // "status": "open",
+  allIssueContainer.innerHTML = '';
  
   allIssues.forEach(issues => {
     const div = document.createElement('div');
@@ -45,7 +42,7 @@ const displayAllIssues = allIssues => {
 
           <h2 class="text-xl font-semibold mt-3">${issues.title} </h2>
           <h4 class="text- font-medium mb-3 line-clamp-2 text-[#64748B]">${issues.description}</h4>
-
+          <h2 class="text-xl font-semibold mt-3">${issues.status} </h2>
           <div class="flex gap-2">
           <button class="flex items-center bg-[#FECACA] text-[#EF4444] py-2 px-3 rounded-3xl font-semibold"> ${issues.labels[0]}</button>
           <button class=" bg-[#FDE68A] text-[#D97706] px-3 py-2 rounded-3xl font-semibold flex items-center gap-1">${issues.labels[1]}</button>
@@ -66,3 +63,28 @@ const displayAllIssues = allIssues => {
 };
 
 loadAllIssues();
+
+
+// issues filtering
+// all button
+const allbtn = document.getElementById('open-btn');
+allbtn.addEventListener('click', () => {
+  displayAllIssues(allIssues);
+});
+// Open button
+const openBtn = document.getElementById('open-btn');
+openBtn.addEventListener('click', () => {
+  const openIssues = allIssues.filter(
+    issue => issue.status.toLowerCase() === 'open',
+  );
+  displayAllIssues(openIssues); // filtered display
+});
+
+// Close button
+const closeBtn = document.getElementById('close-btn');
+closeBtn.addEventListener('click', () => {
+  const closedIssues = allIssues.filter(
+    issue => issue.status.toLowerCase() === 'closed',
+  );
+  displayAllIssues(closedIssues);
+});
